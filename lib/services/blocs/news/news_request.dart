2438836290ed72bdfc30298993e8d48db231a997
Matthew <adetoyesematthew97@gmail.com>
2022-05-04 21:27:54 +0100

    task done

--- lib/services/blocs/news/news_request.dart
+++ lib/services/blocs/news/news_request.dart
@@ -1,5 +1,3 @@
-// https://newsapi.org/v2/top-headlines?country=us&apiKey=40333007a04c468eb9102b3c127f0c72
-
 import 'package:flutter/cupertino.dart';
 import 'package:new_api_task/models/newModels.dart';
 import 'package:new_api_task/services/apis/constants/constants.dart';
@@ -10,9 +8,9 @@ import '../../apis/api_fetch.dart';
 
 fetchNews(BuildContext? context) async {
   var url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=$API_KEY";
-  var btcnews = await get(url: url, context: context);
+  var usnews = await get(url: url, context: context);
   NewsBLoc newsBLoc = locator.get<NewsBLoc>();
-  List<NewsModel> mnews = btcnews['data']['articles']
+  List<NewsModel> mnews = usnews['data']['articles']
       .map<NewsModel>((news) => NewsModel.fromJson(news))
       .toList();
   newsBLoc.addBanks(mnews);
